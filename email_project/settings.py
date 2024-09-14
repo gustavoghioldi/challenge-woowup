4585c2ca5cfd12f82fe0f3e2471791cf42b7f3ca
Barba Satan <gustavoghioldi@gmail.com>
2024-09-14 04:28:23 -0300

    render configuration

--- email_project/settings.py
+++ email_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -87,14 +88,11 @@ WSGI_APPLICATION = "email_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "mydatabase"),
-        "USER": os.getenv("POSTGRES_USER", "myuser"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "mypassword"),
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
-        "PORT": os.getenv("POSTGRES_PORT", 5432),
-    }
+       'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://myuser:mypassword@db:5432/mydatabase',
+        conn_max_age=600
+    )
 }
 # DATABASES = {
 #     "default": {
